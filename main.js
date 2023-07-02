98363aa9d2ffac80b077f2a92671bdef372d96e8
Vibhu Gupta <vik6026717961@gmail.com>
2023-07-03 04:44:48 +0530

    sidebar And testmonial

--- main.js
+++ main.js
@@ -2,3 +2,21 @@
 window.addEventListener('scroll',() =>{
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
 })
+
+
+// show/hide faq answer
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open')
+
+        //change icons
+        // answer.nextElementSibling.classList.toggle('active')
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className == 'ri-add-fill'){
+            icon.className = 'ri-subtract-fill';
+        }else {
+            icon.className = 'ri-add-fill';
+        }
+    })
+})
